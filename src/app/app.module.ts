b63c958ef37ed77991b88410d0595e762aa2b660
darwin <darwinbedoya05@gmail.com>
2023-01-14 20:47:31 -0500

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import { PortafolioComponent } from './portafolio/portafolio.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    
   ],
   imports: [
     BrowserModule,
--- src/app/portafolio/portafolio.component.ts
+++ src/app/portafolio/portafolio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as copy from 'copy-to-clipboard';
 
 @Component({
   selector: 'app-portafolio',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./portafolio.component.scss']
 })
 export class PortafolioComponent {
+
+
+  copyEmail(text: string) {
+    console.log(text);
+    copy(text, {
+    });
+  };
 
 }
